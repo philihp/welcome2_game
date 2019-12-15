f5582d3386384166c59a9ff93c3acf8ccbdd6137
Philihp Busby <philihp@gmail.com>
2019-12-15 07:10:15 +0000

    marks slots used when plan is applied

--- lib/welcome2_game/estate_planner.ex
+++ lib/welcome2_game/estate_planner.ex
@@ -1,5 +1,11 @@
 defmodule Welcome2Game.EstatePlanner do
-  alias Welcome2Game.{State, Plan}
+  alias Welcome2Game.{State, Plan, Tableau}
+
+  @houses_per_row %{
+    a: 10,
+    b: 11,
+    c: 12
+  }
 
   def update(
         state = %State{
@@ -12,12 +18,12 @@ defmodule Welcome2Game.EstatePlanner do
         }
       ) do
     state
-    |> apply_plan(plan1, plan1_used)
-    |> apply_plan(plan2, plan2_used)
-    |> apply_plan(plan3, plan3_used)
+    |> maybe_apply_plan(plan1, plan1_used)
+    |> maybe_apply_plan(plan2, plan2_used)
+    |> maybe_apply_plan(plan3, plan3_used)
   end
 
-  def apply_plan(state = %State{}, plan, used) do
+  def maybe_apply_plan(state = %State{}, plan, used) do
     (!used && satisfy_plan?(state, plan) && with_plan(state, plan)) || state
   end
 
@@ -29,7 +35,68 @@ defmodule Welcome2Game.EstatePlanner do
     )
   end
 
-  def with_plan(state, _plan) do
-    state
+  def with_plan(state, plan) do
+    Enum.reduce(plan_needs(plan), state, fn blocksize, state ->
+      {row, index} = first_estate_at(state, blocksize)
+      block_off(state, blocksize, row, index)
+    end)
+  end
+
+  def plan_needs(%Plan{needs: needs}) do
+    for {size, amount} <- needs do
+      for _ <- 1..amount do
+        size
+      end
+    end
+    |> List.flatten()
+    |> Enum.reverse()
+  end
+
+  def first_estate_at(state, blocksize) do
+    first_estate_in(state, blocksize, :a) ||
+      first_estate_in(state, blocksize, :b) ||
+      first_estate_in(state, blocksize, :c)
+  end
+
+  def first_estate_in(state, blocksize, row) do
+    found =
+      Enum.find(1..@houses_per_row[row], nil, fn index ->
+        estate_exists_at({row, index}, state, blocksize)
+      end)
+
+    found && {row, found}
+  end
+
+  def estate_exists_at({row, index}, %State{player: player}, 1) do
+    Enum.all?([
+      Map.get(player, :"fence#{row}#{index - 1}", true),
+      Map.get(player, :"row#{row}#{index}number", nil) !== nil,
+      Map.get(player, :"row#{row}#{index}plan", true) === false,
+      Map.get(player, :"fence#{row}#{index}", true)
+    ])
+  end
+
+  def estate_exists_at({row, index}, %State{player: player}, blocksize) do
+    Enum.all?(
+      [
+        # left fence built
+        Map.get(player, :"fence#{row}#{index - 1}", true),
+        # right fence built
+        Map.get(player, :"fence#{row}#{index - 1 + blocksize}", true)
+      ] ++
+        for offset <- 0..(blocksize - 1) do
+          Map.get(player, :"row#{row}#{index + offset}number", nil) !== nil &&
+            Map.get(player, :"row#{row}#{index + offset}plan", true) !== true
+        end ++
+        for offset <- 0..(blocksize - 2) do
+          Map.get(player, :"fence#{row}#{index + offset}", true) === false
+        end
+    )
+  end
+
+  def block_off(state, blocksize, row, index) do
+    Enum.reduce(0..(blocksize - 1), state, fn offset, state ->
+      %State{state | player: struct(state.player, %{:"row#{row}#{index + offset}plan" => true})}
+    end)
   end
 end
--- test/welcome2_game/estate_planner_test.exs
+++ test/welcome2_game/estate_planner_test.exs
@@ -65,4 +65,114 @@ defmodule Welcome2Game.EstatePlannerTest do
       assert false == EstatePlanner.satisfy_plan?(state, state.plan1)
     end
   end
+
+  describe "#plan_needs" do
+    test "returns empty array" do
+      result = EstatePlanner.plan_needs(%Plan{needs: %{}})
+      assert result == []
+    end
+
+    test "returns an array of estate sizes to find" do
+      result = EstatePlanner.plan_needs(%Plan{needs: %{4 => 3, 3 => 2, 2 => 1}})
+      assert result == [4, 4, 4, 3, 3, 2]
+    end
+
+    test "plan of just one type" do
+      result = EstatePlanner.plan_needs(%Plan{needs: %{3 => 3}})
+      assert result == [3, 3, 3]
+    end
+  end
+
+  describe "#first_estate_at" do
+    test "" do
+      player = %Tableau{
+        rowa1number: 1,
+        rowa2number: 2,
+        rowa3number: 3,
+        rowa4number: 4,
+        fencea4: true,
+        rowa5number: 5,
+        rowa6number: 6,
+        rowa7number: 7,
+        fencea7: true,
+        rowa8number: 8,
+        rowa9number: 9,
+        rowa10number: 10
+      }
+
+      # assert EstatePlanner.first_estate_at(%State{player: player}, 3) == {:a, 5}
+      # assert EstatePlanner.first_estate_at(%State{player: player}, 4) == {:a, 1}
+      assert EstatePlanner.first_estate_at(%State{player: player}, 5) == nil
+    end
+  end
+
+  describe "#estate_exists_at" do
+    test "a few found" do
+      player = %Tableau{
+        rowa1number: 1,
+        rowa2number: 2,
+        rowa3number: 3,
+        rowa4number: 4,
+        fencea4: true,
+        rowa5number: 5,
+        rowa6number: 6,
+        rowa7number: 7,
+        fencea7: true,
+        rowa8number: 8,
+        rowa9number: 9,
+        rowa10number: 10
+      }
+
+      assert EstatePlanner.estate_exists_at({:a, 1}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 2}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 3}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 4}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 5}, %State{player: player}, 3) == true
+      assert EstatePlanner.estate_exists_at({:a, 6}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 7}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 8}, %State{player: player}, 3) == true
+      assert EstatePlanner.estate_exists_at({:a, 9}, %State{player: player}, 3) == false
+      assert EstatePlanner.estate_exists_at({:a, 10}, %State{player: player}, 3) == false
+    end
+
+    test "none found of too big" do
+      player = %Tableau{
+        rowa1number: 1,
+        rowa2number: 2,
+        rowa3number: 3,
+        rowa4number: 4,
+        fencea4: true,
+        rowa5number: 5,
+        rowa6number: 6,
+        rowa7number: 7,
+        fencea7: true,
+        rowa8number: 8,
+        rowa9number: 9,
+        rowa10number: 10
+      }
+
+      assert EstatePlanner.estate_exists_at({:a, 1}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 2}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 3}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 4}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 5}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 6}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 7}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 8}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 9}, %State{player: player}, 5) == false
+      assert EstatePlanner.estate_exists_at({:a, 10}, %State{player: player}, 5) == false
+    end
+  end
+
+  describe "#block_off" do
+    test "blocks off a set of slots" do
+      state = EstatePlanner.block_off(%State{player: %Tableau{}}, 3, :b, 5)
+
+      assert state.player.rowb4plan == false
+      assert state.player.rowb5plan == true
+      assert state.player.rowb6plan == true
+      assert state.player.rowb7plan == true
+      assert state.player.rowb8plan == false
+    end
+  end
 end
